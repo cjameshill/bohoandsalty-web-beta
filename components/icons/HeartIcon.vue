ba7538e07dceffb9cf39d31fe633212b9aa8c81f
Christopher Hill <cjameshill@gmail.com>
2023-01-25 14:57:16 +0100

    Added vibrate on click

--- components/icons/HeartIcon.vue
+++ components/icons/HeartIcon.vue
@@ -1,7 +1,14 @@
 <script setup>
 import { onMounted, ref } from "vue";
 import { create } from "@lottiefiles/lottie-interactivity";
+import { useVibrate } from "@vueuse/core";
+const { vibrate } = useVibrate({ pattern: [200] });
+
 const lottie = ref();
+const click = () => {
+  console.log("click");
+  vibrate();
+};
 onMounted(() => {
   lottie.value.addEventListener("load", function () {
     create({
@@ -21,6 +28,7 @@ onMounted(() => {
     id="toggleLottie"
     ref="lottie"
     style="width: 50px"
+    @click="click"
     class="cursor-pointer rounded-full active:bg-slate-100"
     src="https://assets8.lottiefiles.com/packages/lf20_l5g0dsej.json"
     speed="3"
